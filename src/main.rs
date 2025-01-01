4018df3d034dc94a7e1f78d85310da7f23aadc07
mahdyfalah <mahdyfalah@gmail.com>
2025-01-01 21:51:57 +0100

    import service

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
+use ray_tracing::service::scene_import_service::SceneImportService;
+
 fn main() {
-    // match SceneImportService::import_scene() {
-    //     Some(scene) => {
-    //         println!("Scene imported successfully: {:?}", scene);
-    //     }
-    //     None => {
-    //         println!("Failed to import the scene.");
-    //     }
-    // }
+    match SceneImportService::import_scene() {
+        Ok(scene) => println!("Scene imported successfully: {:?}", scene),
+        Err(err) => eprintln!("Error: {}", err),
+    }
 }
--- src/service/scene_import_service.rs
+++ src/service/scene_import_service.rs
@@ -1,51 +1,45 @@
-#![allow(unused)]
-
 use rfd::FileDialog;
 use std::fs::File;
-use std::io::Read;
-use serde::Deserialize;
+use std::io::{Read};
+use std::path::Path;
 use serde_xml_rs::from_str;
+use crate::models::scene::Scene;
 
 #[derive(Debug)]
 pub struct SceneImportService;
 
 impl SceneImportService {
-    // /// Opens a file dialog to select an XML file and parses it into a Scene object.
-    // pub fn import_scene() -> Option<Scene> {
-    //     // Open file dialog
-    //     let file = FileDialog::new()
-    //         .add_filter("XML files", &["xml"])
-    //         .pick_file();
-    //
-    //     match file {
-    //         Some(path) => {
-    //             println!("Selected file: {:?}", path);
-    //             // Attempt to read and parse the file
-    //             match Self::parse_scene_from_file(&path) {
-    //                 Ok(scene) => Some(scene),
-    //                 Err(err) => {
-    //                     eprintln!("Error parsing scene: {:?}", err);
-    //                     None
-    //                 }
-    //             }
-    //         }
-    //         None => {
-    //             println!("No file selected");
-    //             None
-    //         }
-    //     }
-    // }
-    //
-    // /// Parses a Scene object from the provided file path
-    // fn parse_scene_from_file(path: &std::path::Path) -> Result<Scene, Box<dyn std::error::Error>> {
-    //     let mut file = File::open(path)?;
-    //     let mut content = String::new();
-    //     file.read_to_string(&mut content)?;
-    //
-    //     // Deserialize just the background color for now
-    //     let scene: Scene = serde_xml_rs::from_str(&content)?;
-    //
-    //     // Here you would set a default camera if needed, for now we set it to None
-    //     Ok(scene)
-    // }
+    /// Opens a file dialog to select an XML file and parses it into a Scene object.
+    pub fn import_scene() -> Result<Scene, String> {
+        // Open file dialog
+        let file = FileDialog::new()
+            .add_filter("XML files", &["xml"])
+            .pick_file();
+
+        match file {
+            Some(path) => {
+                println!("Selected file: {:?}", path);
+
+                // Attempt to parse the scene
+                match Self::parse_scene_from_file(&path) {
+                    Ok(scene) => Ok(scene),
+                    Err(err) => Err(format!("Failed to parse scene: {}", err)),
+                }
+            }
+            None => Err(String::from("No file selected")),
+        }
+    }
+
+    /// Parses a Scene object from the provided file path
+    fn parse_scene_from_file(path: &Path) -> Result<Scene, Box<dyn std::error::Error>> {
+        // Read the file content
+        let mut file = File::open(path)?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+
+        // Deserialize the scene from XML
+        let scene: Scene = from_str(&content)?;
+
+        Ok(scene)
+    }
 }
