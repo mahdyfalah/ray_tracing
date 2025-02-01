7c7467318c07020d6cf8609019c95957a627532a
mahdyfalah <mahdyfalah@gmail.com>
2025-02-02 00:46:06 +0100

    small fixes

--- src/models/scene.rs
+++ src/models/scene.rs
@@ -1,13 +1,14 @@
 use std::io;
 use serde::Deserialize;
 use crate::models::camera::Camera;
+use crate::models::color::Color;
 use crate::models::lights::Lights;
 use crate::models::surface::{Surfaces, SurfaceType}; // Import from surface.rs
 
 #[derive(Debug, Deserialize, PartialEq)]
 pub struct Scene {
     pub output_file: String,
-    pub background_color: crate::models::color::Color,
+    pub background_color: Color,
     pub camera: Camera,
     pub lights: Lights,
     pub surfaces: Surfaces,
